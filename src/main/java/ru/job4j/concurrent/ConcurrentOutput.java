026f808bf3c81a4252901e9e73ac00c5057abd7d
Arngrame <arngrame.dev@gmail.com>
2025-05-14 08:48:23 +0400

    Create and start thread

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,13 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Runnable runnable = () -> System.out.println(Thread.currentThread().getName());
+        Thread another = new Thread(runnable);
+        Thread second = new Thread(runnable);
+
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
