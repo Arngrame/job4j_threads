df3eee96c68834898a592b555b77fda529e40070
Arngrame <arngrame.dev@gmail.com>
2025-05-14 12:10:23 +0400

    Thread sleep fix

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -4,9 +4,7 @@ public class Wget {
     public static void main(String[] args) {
         try {
             for (int index = 0; index <= 100; index++) {
-                // \r - возврат каретки в начало строки для её обновления через промежутки времени
                 System.out.print("\rLoading: " + index + "%");
-                // 1000ms = 1s
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
