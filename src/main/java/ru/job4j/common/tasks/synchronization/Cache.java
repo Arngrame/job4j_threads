caeed5e732c965d16deb7783705e24205287813e
Arngrame <arngrame.dev@gmail.com>
2025-05-14 16:52:30 +0400

    Synchronization of common resources

---
+++ src/main/java/ru/job4j/common/tasks/synchronization/Cache.java
@@ -0,0 +1,12 @@
+package ru.job4j.common.tasks.synchronization;
+
+public final class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache getInstance() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
