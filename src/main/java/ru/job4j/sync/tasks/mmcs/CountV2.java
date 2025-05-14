8b69c2a7e8aaf4efa01733d75af85e0ecf8820ee
Arngrame <arngrame.dev@gmail.com>
2025-05-14 22:50:35 +0400

    Mutex, monitor, critical section

---
+++ src/main/java/ru/job4j/sync/tasks/mmcs/CountV2.java
@@ -0,0 +1,17 @@
+package ru.job4j.sync.tasks.mmcs;
+
+public class CountV2 {
+    private int value;
+
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
+    }
+
+    public int get() {
+        synchronized (this) {
+            return value;
+        }
+    }
+}
