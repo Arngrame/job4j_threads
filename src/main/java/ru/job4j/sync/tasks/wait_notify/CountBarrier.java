2616ec4f08be20f010d4897a40ddf12ab632a8f9
Arngrame <arngrame.dev@gmail.com>
2025-05-16 19:01:44 +0400

    wait, notify, notifyAll task completed

---
+++ src/main/java/ru/job4j/sync/tasks/wait_notify/CountBarrier.java
@@ -0,0 +1,57 @@
+package ru.job4j.sync.tasks.wait_notify;
+
+public class CountBarrier {
+
+    private final Object monitor = this;
+    private final int total;
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            System.out.println(Thread.currentThread().getName() + " увеличил счётчик, текущее значение = " + count);
+
+            if (count >= total) {
+                System.out.println("Выполняется пробуждение остальных потоков...");
+                notifyAll();
+            }
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    System.out.println("Сплю пока счётчик не достиг нужного значения.");
+                    System.out.println("Текущее значение = " + count + ", необходимо: " + total);
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(5);
+
+        new Thread(() -> {
+            System.out.println("Жду обновление счётчика...");
+            countBarrier.await();
+            System.out.println("Счётчик обновлён до нужного значения");
+        }, "t1").start();
+
+        new Thread(() -> {
+            countBarrier.count();
+            countBarrier.count();
+            countBarrier.count();
+            countBarrier.count();
+            countBarrier.count();
+        }, "t2").start();
+    }
+}
